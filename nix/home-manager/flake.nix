5b7cd2184a7238f874090c5413d83544a807f391
coolGi <sasanaps@hotmail.com>
2024-05-24 21:21:48 +0930

    added nixgl flake

--- nix/home-manager/flake.nix
+++ nix/home-manager/flake.nix
@@ -2,10 +2,11 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
+    nixgl.url = "github:nix-community/nixGL";
     stylix.url = "github:danth/stylix";
   };
 
-  outputs = { nixpkgs, home-manager, stylix, ... }: {
+  outputs = { nixpkgs, home-manager, nixgl, stylix, ... }: {
     homeConfigurations."coolgi" = home-manager.lib.homeManagerConfiguration {
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
       modules = [ stylix.homeManagerModules.stylix ./home.nix ];
