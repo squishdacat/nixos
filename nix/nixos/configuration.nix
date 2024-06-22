42b052b7b5aafd151725a1ca0bba13d69331bce0
coolGi <sasanaps@hotmail.com>
2024-06-22 19:39:37 +0930

    undescovering something that doesnt exist

--- nix/nixos/configuration.nix
+++ nix/nixos/configuration.nix
@@ -20,7 +20,7 @@
 
 
   # The set of english words (at $WORDLIST env var)
-  environment.wordList.enable = true;
+  #environment.wordList.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
