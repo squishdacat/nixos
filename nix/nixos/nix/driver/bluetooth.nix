4d962ce6f9c37b977daf636a3af653f89afb3bc3
coolGi <sasanaps@hotmail.com>
2024-06-23 20:55:39 +0930

    replaced bluezFull with bluez

--- nix/nixos/nix/driver/bluetooth.nix
+++ nix/nixos/nix/driver/bluetooth.nix
@@ -4,8 +4,6 @@
     enable = true;
     powerOnBoot = false; # Whether to power up the bluetooth controller on boot
 
-    package = pkgs.bluezFull;
-
     settings = {
       General.Experimental = true; # Show battery level
     };
