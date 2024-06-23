b847e4446aeb50dbcdc8f3d883fbf8434253505d
coolGi <sasanaps@hotmail.com>
2024-06-23 13:41:38 +0930

    added extra bluetooth options

--- nix/nixos/nix/driver/bluetooth.nix
+++ nix/nixos/nix/driver/bluetooth.nix
@@ -1,7 +1,14 @@
-{ ... }:
+{ pkgs, ... }:
 {
   hardware.bluetooth = {
     enable = true;
     powerOnBoot = false; # Whether to power up the bluetooth controller on boot
+
+    package = pkgs.bluezFull;
+
+    settings = {
+      General.Experimental = true; # Show battery level
+    };
   };
+  services.blueman.enable = true;
 }
