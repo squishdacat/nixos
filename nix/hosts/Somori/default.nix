282e5cad82b7b418a990654e16e5db06f8d34f21
coolGi <sasanaps@hotmail.com>
2024-07-24 19:10:35 +0000

    added nameservers incase i need it

--- nix/hosts/Somori/default.nix
+++ nix/hosts/Somori/default.nix
@@ -27,6 +27,28 @@
     };
   };
 
+  #networking = {
+  #  nameservers = [
+  #    ##### Porkbun Name Servers #####
+  #    # alvador.ns.porkbun.com
+  #    "162.159.10.150"
+  #    "2400:cb00:2049:1::a29f:a96"
+  #    # curitiba.ns.porkbun.com
+  #    "173.245.58.37"
+  #    "2400:cb00:2049:1::adf5:3a25"
+  #    # fortaleza.ns.porkbun.com
+  #    "162.159.8.140"
+  #    "2400:cb00:2049:1::a29f:88c"
+  #    # maceio.ns.porkbun.com
+  #    "162.159.11.180"
+  #    "2400:cb00:2049:1::a29f:bb4"
+  #  ];
+  #  # If using dhcpcd:
+  #  dhcpcd.extraConfig = "nohook resolv.conf";
+  #  # If using NetworkManager:
+  #  networkmanager.dns = "none";
+  #};
+
   services.nginx = {
     enable = true;
 
