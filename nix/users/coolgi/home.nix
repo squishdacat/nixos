000f79b37cf121362125cc8d0b115c7d6bff1d27
coolGi <me@coolgi.dev>
2024-08-04 23:33:33 +0930

    updated flakes

--- nix/users/coolgi/home.nix
+++ nix/users/coolgi/home.nix
@@ -1,14 +1,5 @@
 { ... }:
 {
-  home = {
-    username = "coolgi";
-    homeDirectory = "/home/coolgi";
-    stateVersion = "24.05";
-    preferXdgDirectories = true;
-  };
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
-
   imports = [
     ./../../home-manager/home.nix
 
