7348e5e3b6ce793b21dc5ee21a4a08531d01cc04
coolGi <me@coolgi.dev>
2024-08-15 03:41:10 +0000

    fixed ipv6 networking

--- nix/hosts/Somori/networking.nix
+++ nix/hosts/Somori/networking.nix
@@ -1,6 +1,10 @@
 { ... }:
 {
   networking = {
+    # We are using systemd.network instead
+    useDHCP = false;
+
+
     nameservers = [
       ##### Porkbun Name Servers #####
       # alvador.ns.porkbun.com
@@ -16,25 +20,30 @@
       "162.159.11.180"
       "2400:cb00:2049:1::a29f:bb4"
     ];
-    # If using dhcpcd:
-    #dhcpcd.extraConfig = "ipv4";
-    # If using NetworkManager:
-    #networkmanager.dns = "none";
   };
 
   # This make sure that our interface is named `eth0`.
-  # This should be ok as long as you don't have multiple physical network cards
+  # This should be ok as long as there arent multiple physical network cards
   # For multiple cards one could add a netdev unit to rename the interface based on the mac address
   networking.usePredictableInterfaceNames = false;
 
   systemd.network = {
-    #enable = true;
+    enable = true;
 
-    networks."eth0" = {
+    # Use the vendors thingie for IPv4 DHCP
+    config.dhcpV4Config = { DUIDType = "vendor"; };
+
+    networks."Somori-contabo" = {
       enable = true;
 
+      # Interface name
       name = "eth0";
 
+      # Use the vendors thingie for IPv4 DHCP
+      dhcpV4Config = { UseDNS = true; UseRoutes = true; };
+
+
+      # Somori's spesific network settings
       address = [
         "185.193.17.204/24"
 
@@ -61,7 +70,7 @@
         }
         {
           Gateway = "fe80::1";
-          Destination = "2a02:c206:2202:9155:0000:0000:0000:0001";
+          Destination = "2a02:c206:2202:9155:0000:0000:0000:0001/64";
           GatewayOnLink = true;
         }
       ];
