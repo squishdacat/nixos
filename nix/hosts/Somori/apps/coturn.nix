ab27e62508c364bb473bb1df741026877b0879e3
coolGi <me@coolgi.dev>
2024-09-22 13:50:02 +0930

    attempted to add coturn to the server

---
+++ nix/hosts/Somori/apps/coturn.nix
@@ -0,0 +1,109 @@
+{ config, ... }:
+{
+  # Firewall for this
+  networking.firewall = 
+    let
+      range = with config.services.coturn; [
+        {
+          from = min-port;
+          to = max-port;
+        }
+      ];
+    in
+    {
+      allowedUDPPortRanges = range;
+      allowedUDPPorts = [ 3478 5349 ];
+      allowedTCPPortRanges = [ ];
+      allowedTCPPorts = [ 3478 5349 ];
+    };
+
+  services.coturn = {
+    enable = true;
+
+    realm = "turn.coolgi.dev";
+
+    use-auth-secret = true;
+    static-auth-secret-file = "/etc/matrix/coturn_key";
+
+    min-port = 49000;
+    max-port = 50000;
+    listening-port = 3478;
+
+    # VoIP traffic is all UDP. There is no reason to let users connect to arbitrary TCP endpoints via the relay.
+    no-tcp-relay = true;
+    # I'm not gonna be needing the cli
+    no-cli = true;
+
+    #cert = "/var/lib/acme/turn.coolgi.dev/full.pem";
+    #pkey = "/var/lib/acme/turn.coolgi.dev/key.pem";
+
+    /*
+    extraConfig = ''
+      # Ban private IP ranges
+      # see https://www.rtcsec.com/article/slack-webrtc-turn-compromise-and-bug-bounty/#how-to-fix-an-open-turn-relay-to-address-this-vulnerability
+      # and https://nixos.wiki/wiki/Matrix
+      no-multicast-peers
+      denied-peer-ip=0.0.0.0-0.255.255.255
+      denied-peer-ip=10.0.0.0-10.255.255.255
+      denied-peer-ip=100.64.0.0-100.127.255.255
+      denied-peer-ip=127.0.0.0-127.255.255.255
+      denied-peer-ip=169.254.0.0-169.254.255.255
+      denied-peer-ip=172.16.0.0-172.31.255.255
+      denied-peer-ip=192.0.0.0-192.0.0.255
+      denied-peer-ip=192.0.2.0-192.0.2.255
+      denied-peer-ip=192.88.99.0-192.88.99.255
+      denied-peer-ip=192.168.0.0-192.168.255.255
+      denied-peer-ip=198.18.0.0-198.19.255.255
+      denied-peer-ip=198.51.100.0-198.51.100.255
+      denied-peer-ip=203.0.113.0-203.0.113.255
+      denied-peer-ip=240.0.0.0-255.255.255.255
+      denied-peer-ip=::1
+      denied-peer-ip=64:ff9b::-64:ff9b::ffff:ffff
+      denied-peer-ip=::ffff:0.0.0.0-::ffff:255.255.255.255
+      denied-peer-ip=100::-100::ffff:ffff:ffff:ffff
+      denied-peer-ip=2001::-2001:1ff:ffff:ffff:ffff:ffff:ffff:ffff
+      denied-peer-ip=2002::-2002:ffff:ffff:ffff:ffff:ffff:ffff:ffff
+      denied-peer-ip=fc00::-fdff:ffff:ffff:ffff:ffff:ffff:ffff:ffff
+      denied-peer-ip=fe80::-febf:ffff:ffff:ffff:ffff:ffff:ffff:ffff
+    '';
+    */
+  };
+
+  # get a certificate
+  /*
+  security.acme.certs = {
+    "coolgi.dev" = {
+      reloadServices = [ "coturn.service" ];
+      extraDomainNames = [ "turn.coolgi.dev" ];
+    };
+    /*
+    ${config.services.coturn.realm} = {
+      postRun = "systemctl restart coturn.service";
+      group = "turnserver";
+    };
+    * /
+  };
+  */
+  /*
+  services.nginx.streamConfig = ''
+    map $ssl_preread_server_name $name {
+        coolgi.dev url_backend;
+        turn.coolgi.dev turn_server;
+    }
+
+    upstream url_backend {
+        server 127.0.0.1:4444;
+    }
+    upstream turn_server {
+        server 127.0.0.1:3478;
+    }
+
+    server {
+      listen 443;
+      listen [::]:443;
+      ssl_preread on;
+      proxy_pass $name;
+    }
+  '';
+  */
+}
