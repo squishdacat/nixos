5beba79fdbb30e82d1094c58ae15ac6cb2c5b7f7
coolGi <me@coolgi.dev>
2024-11-04 00:39:00 +1030

    added user keys to public dotfiles

--- nix/users/coolgi/os.nix
+++ nix/users/coolgi/os.nix
@@ -7,6 +7,12 @@
     # NOTE: Remember to change the password on first login
     initialPassword = "password";
 
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKOuFI0KrlI1ywwcWmrAEVYvAiqwcDAdVYxS0+Og+Qvh coolgi@Dbasil"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGW5ePtcN+/WcyBjZRswx2qV6HSW/YdD1Pp/LW/vq2JG coolgi@Lsunny"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIKVXGo8ZvCycoJswJSuZeGDEQ0lUWy7en5i8wlQ4eSF coolgi@Somori"
+    ];
+
     extraGroups = [
       "networkmanager"
       "wheel"
