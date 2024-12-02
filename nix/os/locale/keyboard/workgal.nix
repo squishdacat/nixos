3691777e54c5fc621ef8e6d205b6da17111d021f
coolGi <me@coolgi.dev>
2024-12-02 17:06:46 +1030

    started working on workgal keyboard config

---
+++ nix/os/locale/keyboard/workgal.nix
@@ -0,0 +1,15 @@
+{ ... }:
+{
+  imports = [ ./default.nix ];
+  services.xserver.xkb = {
+    variant = "workgal";
+  
+    extraLayouts = {
+      workgal = {
+        description = "A high efficency keyboard layout inspired by Workman";
+        languages = [ "eng" ];
+        symbolsFile = ./workgal;
+      };
+    };
+  };
+}
