55e4bfce9292e0bf316e2126bcc12f79c655747e
coolGi <me@coolgi.dev>
2024-12-03 22:08:47 +1030

    made the honeypot look more realistic

--- nix/hosts/Somori/apps/mc-honeypot.nix
+++ nix/hosts/Somori/apps/mc-honeypot.nix
@@ -8,6 +8,7 @@
     enable = true;
     openFirewall = true;
 
+    /*
     settings = {
       port = 25565;
 
@@ -16,6 +17,21 @@
 
       max-players = 69;
       online-players = 420;
+
+      motd = "OwO, attempting to access a server... that doesnt exist >:3";
+      icon-file = ./mc-honeypot-icon.png;
+
+      webhook-url-file = "/etc/webhooks/mc-honeypot";
+    };
+    */
+    settings = {
+      port = 25565;
+
+      version-string = "Paper 1.21.3";
+      protocol-version = 768;
+
+      max-players = 20;
+      online-players = 0;
 
       motd = "OwO, attempting to access a server... that doesnt exist >:3";
       icon-file = ./mc-honeypot-icon.png;
