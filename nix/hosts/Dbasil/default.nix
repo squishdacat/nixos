6a19242f1a77f149dc5fffabff6a3254bb5b7052
coolGi <me@coolgi.dev>
2024-12-08 19:23:23 +1030

    enabled bigscreen vr kernel driver on Dbasil

--- nix/hosts/Dbasil/default.nix
+++ nix/hosts/Dbasil/default.nix
@@ -4,7 +4,7 @@
     ./../defaults/desktop.nix
     #### Drivers ####
     ./../../os/drivers/gpu/amd
-    #./../../os/drivers/vr/bsb
+    ./../../os/drivers/vr/bsb
   ];
 
   networking.firewall.allowedTCPPorts = [ 8123 ];
