74c78d3958c479581e961b1426f426411ec96fd8
coolGi <me@coolgi.dev>
2025-02-22 22:03:49 +1030

    fixed up piper

--- nix/home-manager/app/tts/piper.nix
+++ nix/home-manager/app/tts/piper.nix
@@ -1,8 +1,13 @@
 { pkgs, outputs, ... }:
 let
-  piper = builtins.fetchGit {
+  piper = pkgs.fetchgit {
     url = "https://huggingface.co/rhasspy/piper-voices";
     rev = "293cad0539066f86e6bce3b9780c472cc9157489";
+    sha256 = "sha256-1Uql+Ewa7SMUWkLLOweY2Ehlc2Mz18PnR5btr2REck8=";
+    fetchLFS = true;
+    sparseCheckout = [
+      "en/en_US/libritts_r/medium"
+    ];
   };
 
   libritts_r = "${piper}/en/en_US/libritts_r/medium/en_US-libritts_r-medium.onnx";
@@ -10,7 +15,11 @@ in
 {
   home.file = {
     ".config/speech-dispatcher/modules/piper.conf".text = ''
-      GenericExecuteSynth "echo '$DATA' | ${pkgs.piper-tts}/bin/piper --model ${libritts_r} -s 0 --output_raw | ${pkgs.sox}/bin/sox -r 22050 -c 1 -b 16 -e signed-integer -t raw - -t wav - tempo $RATE pitch $PITCH norm | ${pkgs.alsa-utils}/bin/aplay -t raw -c 1 -r 22050 -f S16_LE"
+      ### GenericExecuteSynth "echo '$DATA' | ${pkgs.piper-tts}/bin/piper --model ${libritts_r} -s 0 --output_raw | ${pkgs.sox}/bin/sox -r 22050 -c 1 -b 16 -e signed-integer -t raw - -t wav - tempo $RATE pitch $PITCH norm | ${pkgs.alsa-utils}/bin/aplay -t raw -c 1 -r 22050 -f S16_LE"
+      GenericExecuteSynth "echo '$DATA'\
+        | ${pkgs.piper-tts}/bin/piper --model ${libritts_r} -s 0 --output_raw\
+        | ${pkgs.sox}/bin/sox -r 22050 -c 1 -b 16 -e signed-integer -t raw - -t wav - tempo $RATE pitch $PITCH norm\
+        | ${pkgs.alsa-utils}/bin/aplay -t raw -c 1 -r 22050 -f S16_LE"
 
       GenericRateAdd 1
       GenericPitchAdd 1
