5c3110fe0df46592ec12697b39af154a4aa30edc
coolGi <me@coolgi.dev>
2025-03-15 22:48:25 +1030

    started working on packing dakanji

---
+++ nix/pkgs/dakanji/default.nix
@@ -0,0 +1,51 @@
+{
+  lib,
+  fetchFromGitHub,
+  flutter324,
+
+  mpv,
+
+  targetFlutterPlatform ? "linux",
+}:
+let
+  pubspecLock = lib.importJSON ./pubspec.lock.json; # cat pubspec.lock | nix run nixpkgs#yj | nix run nixpkgs#jq
+in
+flutter324.buildFlutterApplication (
+  rec {
+    pname = "dakanji-${targetFlutterPlatform}";
+    version = "3.5.0b2";
+
+    allowUnfree = true;
+
+    src = fetchFromGitHub {
+      owner = "CaptainDario";
+      repo = "DaKanji";
+      fetchSubmodules = true;
+      tag = "v${version}";
+      hash = "sha256-caUzky14leSBJ5c4DlXOheLLu43lpPQu5nntmyEYsoM=";
+    };
+
+    inherit pubspecLock;
+
+    gitHashes = {
+      tflite_flutter = "sha256-cYYGZTx8KYf/7tG+XiVQGI7NMC9OAgBeAiYckyk1a7g=";
+    };
+
+    inherit targetFlutterPlatform;
+
+    meta = with lib; {
+      description = "You are learning Japanese? Then you need to try DaKanji!";
+      homepage = "https://dakanji.app/";
+      #license = licenses.;
+      mainProgram = "dakanji";
+      #platforms = [
+      #  "x86_64-linux"
+      #  "aarch64-linux"
+      #];
+      sourceProvenance = [ sourceTypes.fromSource ];
+    };
+  }
+  // lib.optionalAttrs (targetFlutterPlatform == "linux") {
+    #nativeBuildInputs = [ mpv ]; 
+  }
+)
--- nix/pkgs/default.nix
+++ nix/pkgs/default.nix
@@ -4,5 +4,7 @@
 }:
 {
   plymouth-cenco-bounce-theme = pkgs.callPackage ./cenco-bounce { };
+  dakanji = pkgs.callPackage ./dakanji { };
+
   piper-en_US-libritts_r-medium = pkgs.callPackage ./piper/en_US-libritts_r-medium.nix { };
 }
